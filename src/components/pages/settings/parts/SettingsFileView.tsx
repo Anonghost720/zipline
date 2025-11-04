d34df22d5156eba28d04691ec6b45c6770bf5be6
Anonghost720 <anonghost720@users.noreply.github.com>
2025-11-04 04:41:59 +0100

    [SETTINGS]: Update documentation link for variable usage in SettingsFileView component

--- src/components/pages/settings/parts/SettingsFileView.tsx
+++ src/components/pages/settings/parts/SettingsFileView.tsx
@@ -99,7 +99,7 @@ export default function SettingsFileView() {
       <Title order={2}>Viewing Files</Title>
       <Text c='dimmed' mt='xs'>
         All text fields support using{' '}
-        <Anchor target='_blank' href='https://zipline.diced.sh/docs/guides/variables'>
+        <Anchor target='_blank' href='https://docs.gofiber.io/'>
           variables.
         </Anchor>
       </Text>
