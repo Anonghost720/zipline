e4468bfe9dea2ccb2cfb334b97241fcb8d34dea4
Anonghost720 <anonghost720@users.noreply.github.com>
2025-12-14 09:26:41 +0100

    Update variables documentation link to ShareHost docs

--- src/components/pages/settings/parts/SettingsFileView.tsx
+++ src/components/pages/settings/parts/SettingsFileView.tsx
@@ -99,7 +99,7 @@ export default function SettingsFileView() {
       <Title order={2}>Viewing Files</Title>
       <Text c='dimmed' mt='xs'>
         All text fields support using{' '}
-        <Anchor target='_blank' href='https://docs.gofiber.io/'>
+        <Anchor target='_blank' href='https://github.com/Anonghost720/sharehost-docs/blob/main/variables.md'>
           variables.
         </Anchor>
       </Text>
